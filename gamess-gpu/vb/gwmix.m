ee60a8d9a465e21a33fed0e19b3eb4ddcbc8443a
Youri Reijne <y.reijne@gmail.com>
2020-05-20 20:51:00 +0200

    Parallel loops on 1:L 2:N

--- gamess-gpu/vb/gwmix.m
+++ gamess-gpu/vb/gwmix.m
@@ -7,6 +7,7 @@ INCLUDE(common/tractlt)
       dimension w1(n1)
       dimension ig(5,nblock)
       dimension ir(nelec),ic(nelec),supg(*)
+!$acc routine (intpos)
 c  supg(0:n2int)
 c
       val=0.0d0
@@ -21,11 +22,16 @@ c     enddo
         print *, "NBLOCK @@", nblock
       end if
 
+!$acc data copyin(ir,ic) present(supg)
+!$acc& copyin(ig(5,nblock), w1(1:n1), nblock)
       do 51 m=1,nblock
          mend = ig(3,m) + ig(1,m) - 1
          do 41 k=ig(3,m)+1, mend
+!$acc parallel loop reduction(+:val_1)
             do 31 l=ig(3,m), k-1
+!$acc loop reduction(+:val_1)
               do 21 i=ig(3,m)+1, mend
+!$acc loop reduction(+:val_1)
                 do 11 j=ig(3,m), i-1
                   !cikjl: calculate the loop vars
                   ii=i-ig(3,m)+1
@@ -45,14 +51,18 @@ c     enddo
 11                continue
 21             continue
 31         continue
+!$acc end parallel loop
 41       continue
 51    continue
 
       do 690 m=1,nblock-1
         do 590 l=ig(4,m), ig(4,m+1)-1
           do 490 j=ig(3,m), ig(3,m+1)-1
+!$acc parallel loop reduction(+:val_2)
             do 390 n=m+1, nblock
+!$acc loop reduction(+:val_2)
               do 290 k=ig(4,n), ig(4,n)+ig(2,n)-1
+!$acc loop reduction(+:val_2)
                 do 190 i=ig(3,n), ig(3,n)+ig(1,n)-1
                   ! wmix: get the first order cofactor from w1
                   jl=(l-ig(4,m))*(ig(3,m+1)-ig(3,m))+j-ig(3,m)+ig(5,m)
@@ -61,7 +71,7 @@ c     enddo
      1                -ig(3,n)+ig(5,n)
                   ! calc 2nd order cofactor * integral
                   s=w1(jl)*w1(ik)*supg(intpos(ir(i),ic(l),ir(j),ic(k)))
-
+                  
                   ! subtract from total if necessary
                   if (m>=ialfa+1.or.m<=ialfa-1.and.n<=ialfa)then
                     val_2 = val_2 - s
@@ -72,11 +82,14 @@ c     enddo
 190               continue
 290             continue
 390           continue
+!$acc end parallel loop
 490         continue
 590       continue
 690     continue
+!$acc end data
       ! print *, "first:", val_1
       ! print *, "second:", val_2
+!$acc wait
       val = val_1 + val_2
       ! print *, "done", val
       ! stop
@@ -87,6 +100,7 @@ c     enddo
       implicit REAL  (a-h,o-z) , integer   (i-n)
 INCLUDE(common/tractlt)
       dimension supg(0:n2int)
+!$acc exit data delete (supg)
       return
       end
     
@@ -94,11 +108,13 @@ INCLUDE(common/tractlt)
       implicit REAL  (a-h,o-z) , integer   (i-n)
 INCLUDE(common/tractlt)
       dimension supg(0:n2int)
+!$acc enter data copyin(supg)
       return
       end
        
       function intposx(i,j,k,l)
       implicit REAL (a-h,o-z)
+!$acc routine
       intposx=0
       return
       end
