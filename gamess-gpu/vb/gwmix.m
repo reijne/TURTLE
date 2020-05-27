4a5a64309905c086562b6eafff904ba1c9078efb
Youri Reijne <y.reijne@gmail.com>
2020-05-27 07:49:36 +0200

    optimised CPU version, lifted computation from inner loops to reduce, maybe slower

--- gamess-gpu/vb/gwmix.m
+++ gamess-gpu/vb/gwmix.m
@@ -1,88 +1,97 @@
-      subroutine gwmix(ir,ic,ig,nblock,ialfa,w1,supg,nelec,n1,val)
-c     Combines cikjl, pikjl, wmix, gmix, gather, ddot and subvec
-c      from matre3.
+      subroutine gwmix(ir,ic,ig,nblock,ialfa,w1,supg,nelec,n1,sum)
+c     Combines cikjl, pikjl, wmix, gmix, (gather, ddot and subvec)
+c     from matre3.
       implicit REAL  (a-h,o-z) , integer   (i-n)
 c
 INCLUDE(common/tractlt)
       dimension w1(n1)
       dimension ig(5,nblock)
       dimension ir(nelec),ic(nelec),supg(*)
-!$acc routine(intpos)
+c!$acc routine(intpos)
 c  supg(0:n2int)
 c
-      val=0.0d0
+      sum=0.0d0
+      ! som=0.0d0
 c     print *,'remco ig::',nblock,ialfa
 c     do i=1,nblock
 c       print *,(ig(k,i),k=1,5)
 c     enddo
-!$acc data copyin(ir,ic) present(supg)
-!$acc& copyin(ig(5,nblock), w1(1:n1), nblock)
+c!$acc data copyin(ir,ic) present(supg), copy(sum)
+c!$acc& copyin(ig(5,nblock), w1(1:n1), nblock)
       do 51 m=1,nblock
-         !msta = ig(3,m)
-         !mend = ig(3,m) + ig(1,m) - 1
-         do 41 k=ig(3,m)+1, ig(3,m)+ig(1,m)-1
-            do 31 l=ig(3,m), k-1
-              do 21 i=ig(3,m)+1, ig(3,m)+ig(1,m)-1
-!$acc parallel loop private(ii, jj, kk, ll, ig5min, scal)
-!$acc& reduction(+:val)
+        end = ig(3,m)+ig(1,m)-1
+        do 41 k=ig(3,m)+1, end
+c!$acc parallel loop reduction(+:sum)
+          kk=k-ig(3,m)
+          do 31 l=ig(3,m), k-1
+              ll=l-ig(3,m)
+c!$acc loop reduction(+:sum)
+              do 21 i=ig(3,m)+1, end
+c!$acc loop reduction(+:sum) private(ii, jj, kk, ig5min)
+                ii=i-ig(3,m)
+                kki = (kk)*ig(1,m)+ii+ig(5,m)
+                lli = (ll)*ig(1,m)+ii+ig(5,m)
                 do 11 j=ig(3,m), i-1
-                  !cikjl: calculate the loop vars
-                  ! ig3mp = ig(3,m)+1 
-                  ii=i-ig(3,m)
-                  jj=j-ig(3,m)
-                  kk=k-ig(3,m)
-                  ll=l-ig(3,m)
+                !cikjl: calculate the loop vars
+                ! ig3mp = ig(3,m)+1 
+                jj=j-ig(3,m)
 
-                  ig5min=ig(5,m)-1
-                  !jacobi ratio theorem to calculate 2nd-o-cofacs
-                  scal=w1((kk)*ig(1,m)+ii+1+ig5min)*
-     &                 w1((ll)*ig(1,m)+jj+1+ig5min)-
-     &                 w1((ll)*ig(1,m)+ii+1+ig5min)*
-     &                 w1((kk)*ig(1,m)+jj+1+ig5min)
+                !jacobi ratio theorem to calculate 2nd-o-cofacs
+                soo=w1(kki)*w1((ll)*ig(1,m)+jj+ig(5,m))-
+     &              w1(lli)*w1((kk)*ig(1,m)+jj+ig(5,m))
                   !(subvec ipos with ipose) * 2nd-o-cofactor, add to total
-                  ! matrix element val
-                  val=val+scal*(supg(intpos(ir(i),ic(k),ir(j),
-     1            ic(l)))-supg(intpos(ir(i),ic(l),ir(j),ic(k))))
+                  ! matrix element sum
+                sum=sum+soo*(supg(intpos(ir(i),ic(k),ir(j),ic(l)))
+     1              -supg(intpos(ir(i),ic(l),ir(j),ic(k))))
                   !it=it+1 !why is this still here? leftover
 11                continue
-!$acc end parallel loop
 21             continue
 31         continue
+c!$acc end parallel loop
 41       continue
 51    continue
-      ! val_af = val
-      do 690 m=1,nblock-1
-        do 590 l=ig(4,m), ig(4,m+1)-1
-          do 490 j=ig(3,m), ig(3,m+1)-1
-            do 390 n=m+1, nblock
+
+      ! sum_af = sum
+      do 690 m=1, nblock-1
+        m1 = m+1
+        do 590 l=ig(4,m), ig(4,m1)-1
+          do 490 j=ig(3,m), ig(3,m1)-1
+c!$acc parallel loop reduction(+:sum)
+            jl=(l-ig(4,m))*(ig(3,m1)-ig(3,m))+j-ig(3,m)+ig(5,m)
+            do 390 n=m1, nblock
+              in = (ig(3,n)+ig(1,n)-ig(3,n))
+c!$acc loop reduction(+:sum)
               do 290 k=ig(4,n), ig(4,n)+ig(2,n)-1
-!$acc parallel loop private(jl, ik) reduction(+:val)
+                ki = k-ig(4,n)
+c!$acc loop reduction(+:sum) private(jl, ik, soo)
                 do 190 i=ig(3,n), ig(3,n)+ig(1,n)-1
                   ! wmix: get the first order cofactor from w1
-                  jl=(l-ig(4,m))*(ig(3,m+1)-ig(3,m))+j-ig(3,m)+ig(5,m)
                   ! get first order cofactor from w1
-                  ik=(k-ig(4,n))*(ig(3,n)+ig(1,n)-ig(3,n))+i
-     1                -ig(3,n)+ig(5,n)
+                  ik=ki*in+i-ig(3,n)+ig(5,n)
+                  soo = w1(ik) * w1(jl)
                   ! calc 2nd order cofactor * integral calc
                   ! add to total
                   if (m>=ialfa+1.or.m<=ialfa-1.and.n<=ialfa) then
-                    val=val - w1(ik) * w1(jl) *
-     5                  supg(intpos(ir(i),ic(l),ir(j),ic(k)))
+                    sum=sum-soo*supg(intpos(ir(i),ic(l),ir(j),ic(k)))
                   end if
-                    val=val + w1(ik) * w1(jl) *
-     1                  supg(intpos(ir(i),ic(k),ir(j),ic(l)))
+                    sum=sum+soo*supg(intpos(ir(i),ic(k),ir(j),ic(l)))
 190               continue
-!$acc end parallel loop
 290             continue
 390           continue
+c!$acc end parallel loop
 490         continue
 590       continue
 690     continue
-!$acc end data
-      ! print *, "first:", val_af
-      ! print *, "second:", val - val_af
-      ! print *, "done", val
-      ! stop
+
+c!$acc end data
+      ! print*, "2nd done:" , it
+      ! print *, sum
+      ! print *, som
+      ! sum = sum + som
+      ! print *, sum
+      ! print *, "first:", sum_af
+      ! print *, "second:", sum - sum_af
+      ! print *, "done", sum
       return
       end
    
@@ -90,7 +99,7 @@ c     enddo
       implicit REAL  (a-h,o-z) , integer   (i-n)
 INCLUDE(common/tractlt)
       dimension supg(0:n2int)
-!$acc exit data delete (supg)
+c!$acc exit data delete (supg)
       return
       end
     
@@ -98,13 +107,13 @@ INCLUDE(common/tractlt)
       implicit REAL  (a-h,o-z) , integer   (i-n)
 INCLUDE(common/tractlt)
       dimension supg(0:n2int)
-!$acc enter data copyin(supg)
+c!$acc enter data copyin(supg)
       return
       end
        
       function intposx(i,j,k,l)
       implicit REAL (a-h,o-z)
-!$acc routine
+c!$acc routine
       intposx=0
       return
       end
